77c59d71cc74fd6bf014383da0d4196f9839e2c5
regar357 <regar357@naver.com>
2025-12-05 01:30:38 +0900

    food 라우트 추가

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const path = require("path");
+const foodRoutes = require("./routes/food");
 
 const app = express();
 const port = 3001;
 
 app.use(express.static(path.join(__dirname, "../frontend/build")));
+app.use("/food", foodRoutes);
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../frontend/build/index.html"));
