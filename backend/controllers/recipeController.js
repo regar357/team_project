adaf5c32e392cedc1e541a69e80fe8bedcb52c21
regar357 <regar357@naver.com>
2025-12-06 23:51:54 +0900

    레시피 API 수정

--- backend/controllers/recipeController.js
+++ backend/controllers/recipeController.js
@@ -79,7 +79,7 @@ exports.generateRecipe = async (req, res) => {
         ]
       );
 
-      res.json({ message: "레시피 생성 완료", recipe: final_recipe });
+      res.json({ message: "레시피 생성 완료", recipe: result });
     } catch (err) {
       res.status(500).json({ error: "JSON Parse Error", detail: err.message });
     }
@@ -87,74 +87,68 @@ exports.generateRecipe = async (req, res) => {
 };
 
 exports.getRecipeList = async (req, res) => {
-  res.json({
-    message: "레시피 목록 조회 성공",
-  });
+  console.log("목록 조회 성공");
 
-  // try {
-  //   const [rows] = await pool.query(`
-  //     SELECT recipe_id, recipe_title, recipe_description, priority_used_ingredients,
-  //            other_ingredients, servings, recipe_steps, recipe_tips, created_at
-  //     FROM recipe
-  //     ORDER BY created_at DESC
-  //   `);
+  try {
+    const [rows] = await pool.query(`
+      SELECT recipe_id, recipe_title, recipe_description, priority_used_ingredients,
+             other_ingredients, servings, recipe_steps, recipe_tips, created_at
+      FROM recipe
+      ORDER BY created_at DESC
+    `);
 
-  //   res.json({
-  //     message: "레시피 목록 조회 성공",
-  //     recipes: rows,
-  //   });
-  // } catch (err) {
-  //   console.error(err);
-  //   res.status(500).json({ error: "레시피 조회 실패" });
-  // }
+    res.json({
+      message: "레시피 목록 조회 성공",
+      recipes: rows,
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "레시피 조회 실패" });
+  }
 };
 
 exports.getRecipeListByid = async (req, res) => {
   const { id } = req.params;
   console.log("레시피id확인: " + id);
 
-  res.json({
-    message: "레시피 상세보기 확인",
-  });
-  // try {
-  //   const [row] = await pool.query(`
-  //     SELECT recipe_id, recipe_title, recipe_description, priority_used_ingredients,
-  //            other_ingredients, servings, recipe_steps, recipe_tips, created_at
-  //     FROM recipe
-  //     WHERE recipe_id = ?
-  //   `, [id]);
+  try {
+    const [row] = await pool.query(
+      `
+      SELECT recipe_id, recipe_title, recipe_description, priority_used_ingredients,
+             other_ingredients, servings, recipe_steps, recipe_tips, created_at
+      FROM recipe
+      WHERE recipe_id = ?
+    `,
+      [id]
+    );
 
-  //   res.json({
-  //     message: "레시피 목록 조회 성공",
-  //     recipes: row,
-  //   });
-  // } catch (err) {
-  //   console.error(err);
-  //   res.status(500).json({ error: "레시피 조회 실패" });
-  // }
+    res.json({
+      message: "레시피 목록 조회 성공",
+      recipes: row,
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "레시피 조회 실패" });
+  }
 };
 
 exports.deleteRecipe = async (req, res) => {
   const { id } = req.params;
   console.log("레시피id확인: " + id);
 
-  res.json({
-    message: "레시피 삭제 완료",
-    recipe_id: id,
-  });
-  // try {
-  //   const [result] = await pool.query(
-  //     "DELETE FROM recipe WHERE recipe_id = ?",
-  //     [id]
-  //   );
+  try {
+    const [result] = await pool.query(
+      "DELETE FROM recipe WHERE recipe_id = ?",
+      [id]
+    );
 
-  //   if (result.affectedRows === 0) {
-  //     return res.status(404).json({ message: "삭제할 레시피가 없습니다." });
-  //   }
+    if (result.affectedRows === 0) {
+      return res.status(404).json({ message: "삭제할 레시피가 없습니다." });
+    }
 
-  //   return res.json({ message: "레시피 삭제 성공" });
-  // } catch (err) {
-  //   console.error(err);
-  //   res.status(500).json({ error: "레시피 삭제 실패" });
-  // }
+    return res.json({ message: "레시피 삭제 성공" });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "레시피 삭제 실패" });
+  }
 };
